07f29060535a083ae634758b07e0675cac212632
daniilcom <dan19.95@mail.ru>
2022-11-14 12:10:40 +0300

    change add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import { useEffect } from 'react'
 import './App.css'
 
-
-
 function App() {
   useEffect(() => {
     tg.ready()
   }, [])
 
-
-
-  return (
-    <div className="App">
-      work
-
-    </div>
-  )
+  return <div className="App">work</div>
 }
 
 export default App
