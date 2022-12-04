fcbd59e8dad333904d7fd2a7b87bc7cc33a79d71
daniilcom <dan19.95@mail.ru>
2022-12-04 14:27:21 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -22,7 +22,7 @@ const ProductItem = ({ product, className, onAdd }) => {
       <Button className={'add-btn'} onClick={onAddHandler}>
         Выбрать
       </Button>
-      {onAdd !== 0 ? (
+      {onAddHandler !== 0 ? (
         <Button className={'add-btn'} onClick={onAddHandler}>
           Добавлено
         </Button>
